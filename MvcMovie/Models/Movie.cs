277d5542a5579cd5d72a3408025d53fed969ae33
Eshana Milan Weerasinghe <50627267+EshanaWeerasinghe@users.noreply.github.com>
2023-04-11 23:18:23 +0530

    #add new field

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -13,6 +13,8 @@ public class Movie
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
     public string? Genre { get; set; }
-    [Column(TypeName = "decimal(18, 2)")]
+
+    [Column(TypeName = "decimal(18, 4)")]
     public decimal Price { get; set; }
+    public string? Rating { get; set; }
 }
